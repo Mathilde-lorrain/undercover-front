993a6e9eb2d3eff8c5be052477fad28a01ce1689
fabiendv <dallavalle.fabien@gmail.com>
2020-04-16 09:44:03 +0200

    feat: save the current game.

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { backUrl } from '../../variables';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GameService {
   constructor(private http: HttpClient) {}
+  public currentGame: Observable<any>;
+  public game;
 
+  setGame(game) {
+    this.game = game;
+  }
+  getGame() {
+    return this.game;
+  }
   create() {
     return this.http.post<any>(`${backUrl}/api/games`, {});
   }
