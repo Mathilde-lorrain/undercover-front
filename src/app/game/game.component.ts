f665f504bd6310fcfb4b9324ddbcc3781a4333d9
fabiendv <dallavalle.fabien@gmail.com>
2020-04-16 12:37:46 +0200

    feat: add vote function.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from '../services/game.service';
 import { AuthenticationService } from '../services/authentication.service';
 import { User } from '../models/user';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
@@ -15,22 +16,29 @@ export class GameComponent implements OnInit {
   roleType;
   word;
   roundNumber = 1;
-  voteEnable: boolean = false;
+  isVoteEnable: boolean = false;
+  isMyTurn: boolean = false;
   firstInstructions = 'Renseignez un mot lors de votre tour';
   secondInstructions = "Voter contre l'un des joueurs";
   instructions = this.firstInstructions;
+  wordForm: FormGroup;
 
   constructor(
     private authenticationService: AuthenticationService,
-    private gameService: GameService
+    private gameService: GameService,
+    private formBuilder: FormBuilder
   ) {
     this.authenticationService.currentUser.subscribe((x) => (this.user = x));
     // this.gameService.currentGame.subscribe((x) => (this.game = x));
     this.game = this.gameService.getGame();
     console.log('My game');
     console.log(this.game);
-    this.game.roles.map((role) => {
+    this.game.roles.map((role, index) => {
       if (role.user.id === this.user.id) {
+        console.log(index);
+        if (index === 0) {
+          this.isMyTurn = true;
+        }
         this.roleType = role.roleType;
         this.alive = role.alive;
       }
@@ -42,5 +50,22 @@ export class GameComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.wordForm.controls;
+  }
+
+  ngOnInit(): void {
+    this.wordForm = this.formBuilder.group({
+      userWord: ['', Validators.required],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.wordForm.invalid) {
+      return;
+    }
+    console.log('This is my word to send: ');
+    console.log(this.wordForm.value.userWord);
+  }
 }
